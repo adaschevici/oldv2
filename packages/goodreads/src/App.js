2796a0c167f25c35e60a23061118abdc613dce24
Artur Daschevici <a.daschevici@gmail.com>
2020-04-22 15:37:18 +0100

    Remove bundle from source control

--- packages/goodreads/src/App.js
+++ packages/goodreads/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
 import { typography } from '@goodreads-v2/component-library'
 import './App.css'
 
@@ -8,24 +7,9 @@ const { Artifika } = typography
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <main style={{ height: '70vh' }}>
         <Artifika tag="h1">Chuck</Artifika>
       </main>
-      <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>
     </div>
   )
 }
